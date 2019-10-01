fd5f0f4a071329366c96a72a42df22efac4a401e
Yulia Chornenka <blackyulia583@gmail.com>
2019-10-01 17:51:12 +0300

    Add draft Gallery

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+/*Для якорів*/
+
+$(document).ready(function(){
+    $(".top-menu-big-screen").on("click","a", function (event) {
+        event.preventDefault();
+        const $elementClick  = $(this).attr('href');
+        const $destination = $($elementClick).offset().top;
+        $('body,html').animate({
+            scrollTop: $destination
+        }, 1000);
+    });
+});
+
+
+/*Для ініціалізації Tooltip*/
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+
+/*Carousel в Furniture Gallery section*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 6;
+    var totalItems = $('.carousel-item-2').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            if (e.direction=="left") {
+                $('.carousel-item-2').eq(i).appendTo('.carousel-inner-2');
+            }
+            else {
+                $('.carousel-item-2').eq(0).appendTo('.carousel-inner-2');
+            }
+        }
+    }
+});
